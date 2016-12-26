18838c49c2bf3a34f2eeeebbd6c321541d51ec18
Gilad Oved <giladoved@gmail.com>
2016-12-25 20:12:40 -0600

    more changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 (function() {
 	var app = angular.module('App', ['ui.router'])
 	.config(function($sceDelegateProvider, $locationProvider) {
@@ -18,23 +20,36 @@
         	templateUrl: '/src/templates/pages/home/index.html'
         })
         .state('list', {
-        	url: '/list/:type/:name',
+        	url: '/list/:type?/:name?',
         	templateUrl : '/src/templates/pages/list/index.html',
-	    	controller: 'ListCtrl',
-	    	controllerAs: 'list'
+	    	resolve: {
+	    		listService: function($http) {
+	    			return $http.get('/concerts');
+	    		}
+	    	},
+	    	controller: 'ListCtrl'
         })
         .state('watch', {
         	url: '/watch/:id',
 			templateUrl : '/src/templates/pages/watch/index.html',
+			resolve: {
+				watchService: function($http, $stateParams) {
+					var params = {'id': $stateParams.id};
+					return $http.get('/concert', {params: params});
+				}
+			},
 	    	controller: 'WatchCtrl',
 	    	controllerAs: 'watch'
+        })
+        .state('about', {
+        	template: '<h1 style="text-align: center;"> About page </h1>'
         })
         .state('404', {
         	template: '<h1 style="text-align: center;">Page not found</h1>'
         });
 	});
 
-	app.controller('ListCtrl', function($http, $scope, $stateParams) {
+	app.controller('ListCtrl', function(listService, $scope, $stateParams) {
 		var type = $stateParams.type;
 		var name = $stateParams.name;
 
@@ -44,8 +59,41 @@
 		$scope.orderBy = type;
 		$scope.search = name;
 
-		$http.get('/concerts').then((res) => {
-			$scope.concerts = res.data;
+		$scope.concerts = listService.data;
+	});
+
+	app.controller('WatchCtrl', function($scope, $stateParams, watchService) {
+		$scope.id = $stateParams.id;
+		$scope.concert = watchService.data;
+	});
+
+	app.controller('FeaturedCtrl', function($scope, $http) {
+		$http.get('/featured').then((res) => {
+			$scope.concert = res.data;
+		});
+	});
+
+	app.controller('VenuesCtrl', function($http) {
+		$http.get('/venues').then((res) => {
+			this.venues = res.data;
+		});
+	});
+
+	app.controller('GenresCtrl', function($http) {
+		$http.get('/genres').then((res) => {
+			this.genres = res.data;
+		});
+	});
+
+	app.controller('RecentsCtrl', function($http) {
+		$http.get('/recents').then((res) => {
+			this.recents = res.data;
+		});
+	});
+
+	app.controller('RandomCtrl', function($http) {
+		$http.get('/random').then((res) => {
+			this.id = res.data;
 		});
 	});
 
@@ -71,43 +119,7 @@
 			totalSeconds += seconds;
 
 			console.log("Seeking to " + totalSeconds);
-		}
-	});
-
-	app.controller('WatchCtrl', function($http, $scope, $stateParams, $state) {
-		var id = $stateParams.id;
-		$scope.id = id;
-
-		var params = {'id': id}
-		$http.get('/concert', {params: params}).then((res) => {
-			$scope.concert = res.data;
-		}, (res) => {
-		  	$state.go('404');
-		});
-	});
-
-	app.controller('FeaturedCtrl', function($scope, $http) {
-		$http.get('/featured').then((res) => {
-			$scope.concert = res.data;
-		});
-	});
-
-	app.controller('VenuesCtrl', function($http) {
-		$http.get('/venues').then((res) => {
-			this.venues = res.data;
-		});
-	});
-
-	app.controller('GenresCtrl', function($http) {
-		$http.get('/genres').then((res) => {
-			this.genres = res.data;
-		});
-	});
-
-	app.controller('RecentsCtrl', function($http) {
-		$http.get('/recents').then((res) => {
-			this.recents = res.data;
-		});
+		};
 	});
 
 
